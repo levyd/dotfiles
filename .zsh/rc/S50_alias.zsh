a025db75917cd0075392c2746bb6a1a8c082af8a
Dan Levy <dan.v.levy@gmail.com>
2012-05-11 21:44:54 -0300

    Changed alias for vim's less macro to vless.

--- .zsh/rc/S50_alias.zsh
+++ .zsh/rc/S50_alias.zsh
@@ -1,3 +1,3 @@
 alias ll='ls -lh --color=auto --group-directories-first'
 alias vi='vim'
-alias less='/usr/share/vim/vimcurrent/macros/less.sh'
+alias vless='/usr/share/vim/vimcurrent/macros/less.sh'
